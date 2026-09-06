6ee5556115709e8bd3050bbaf6c161ce7968a63a
IvanT <itorresano12@gmail.com>
2026-09-06 17:00:11 +0200

    feat: initial architecture and clean layout for Ramia

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel';
+
+export default defineConfig({
+  output: 'static',
+  adapter: vercel(),
+  integrations: [tailwind()],
+  image: {
+    domains: ['cdn.sanity.io'],
+  },
+  prefetch: {
+    defaultStrategy: 'hover'
+  }
+});
